3145057a13c1ccad6c49ea7d9e4006d73d09436e
Mouna bedoui <Mouna.bedoui@resalogic.com>
2024-10-24 17:57:44 +0200

    first commit

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+
+}
